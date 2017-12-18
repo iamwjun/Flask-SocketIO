04210ed911f8e85933b517471504e8b61f188691
iamk <cddxwujun@qq.com>
2017-12-18 21:31:23 +0800

    socket to sid success

--- app/socket/events.py
+++ app/socket/events.py
@@ -1,52 +1,46 @@
-from flask import request, jsonify
+from flask import request, jsonify, current_app
 from flask_socketio import emit, send, join_room, leave_room
+from datetime import datetime, timedelta
+import time
+import jwt
 from .. import socketio
 from . import socket
-import time
+from ..model.models import User
 
-@socketio.on('message')
-def handle_message(message):
-    print('connect success!')
-    send({'status': '200', 'message': 'success!'})
-
-def ack():
-    print('message was received!')
+@socket.route('/allowLogin', methods=['POST'])
+def allowLogin():
+    try:
+        data = request.get_json()
+        weixin = data['weixin']
+        sid = data['sid']
+        user = User.query.filter_by(weixin=weixin).first()
+        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.utcnow() + timedelta(days=1)}, current_app.config['SECRET_KEY'], algorithm='HS256')
+        print(token)
+        # json = jsonify({'status': '200', 'token': token.decode('UTF-8')})
+        json = {'status': '200', 'token': token.decode('UTF-8')}
+        socketio.send(json, room=sid, namespace='/allow')
+        return jsonify({'status': '200', 'message': 'ok!'})
+    except:
+        return jsonify({'status': '401', 'message': 'an exception occurs!'})
 
 @socketio.on('response', namespace='/test')
 def give_response(data):
-    # print(request.sid)
-    # print('connect success!')
-    # value = data.get('param')
-    # print(value)
     emit('response',{'code':'200','msg':data}, callback=allowLogin)
-    # weixin = 'oCJfqvgk_O60fQ9GIWSp-rrVTiDA'
-    # user = User.query.filter_by(weixin=weixin).first()
-    # token = jwt.encode({'public_id': user.public_id, 'exp': datetime.utcnow() + timedelta(days=1)}, current_app.config['SECRET_KEY'], algorithm='HS256')
     
     time.sleep(15)
     emit('response',{'code':'200','msg':'start to process...'})
-    # emit('response',{'status':'200','token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJsaWNfaWQiOiI0MmNhNDQ5ZGQxYTg0ZTAyYTY5MzIwZmRkZWNlYmI1ZCIsImV4cCI6MTUxMzY1NTIwNX0.XWDbfySWUSae2EcRfMZKJNraTj_KnBo6pETtoTrZ1BA'},callback=ack)
 
-@socketio.on('join')
+@socketio.on('join', namespace='/allow')
 def on_join(data):
-    username = data['username']
+    print(request.sid)
     room = data['room']
+    print(request.sid, room)
     join_room(room)
-    send(username + ' has entered the room.', room=room)
+    send(request.sid + ' has entered the room.', room=room)
 
-@socketio.on('leave')
+@socketio.on('leave', namespace='/allow')
 def on_leave(data):
     username = data['username']
     room = data['room']
     leave_room(room)
     send(username + ' has left the room.', room=room)
-
-@socket.route('/allowLogin', methods=['POST'])
-def allowLogin():
-    # emit('response',{'code':'200','msg':'start to process...'})
-    # join_room(room)
-    # return room
-    data = request.get_json()
-    sid = data['sid']
-    socketio.emit('response', {'data': 'A NEW FILE WAS POSTED'}, room=sid)
-    return jsonify({'message': 'no permission to request!'})
